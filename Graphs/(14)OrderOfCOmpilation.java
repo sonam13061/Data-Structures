040c2abc4383ed004db27e9ba02ed43a68de8bbe
sonam1306 <sonamkhurana589@gmail.com>
2020-06-29 18:03:55 +0530

    graphs updated

--- Graphs/(14)OrderOfCOmpilation.java
+++ Graphs/(14)OrderOfCOmpilation.java
@@ -10,30 +10,50 @@ public class Main {
          this.src = src;
          this.nbr = nbr;
       }
+    }
+    // Sample Input
+    // 7
+    // 7
+    // 0 1
+    // 1 2
+    // 2 3
+    // 0 3
+    // 4 5
+    // 5 6
+    // 4 6
+    // Sample Output
+    // 4
+    // 5
+    // 6
+    // 0
+    // 1
+    // 2
+    // 3
+   public static void OFC(ArrayList<Edge> graph[],int src,boolean[]  visited,Stack<Integer> st){
+       
+        visited[src]=true;
+        for(Edge e:graph[src]){
+            if(!visited[e.nbr]){
+                OFC(graph,e.nbr,visited,st);
+            }
+        }
+        //We are pushing the src in the post area,so the node which will be the on the top would be executed first.
+        st.push(src);
    }
-   public static void OFC(ArrayList<Edge> graph[],int src,boolean[] visited,Stack<Integer> st){
-       visited[src]=true;
-       for(Edge e:graph[src]){
-          if(visited[e.nbr]==false){
-             OFC(graph,e.nbr,visited,st);
-          }
+   public static void OrderOfcompilation(ArrayList<Edge> graph[]){
+       boolean visited[]=new boolean[graph.length];
+       Stack<Integer> st=new Stack<>();
+       for(int i=0;i<graph.length;i++){
+           if(!visited[i]){
+               OFC(graph,i,visited,st);
+           }
        }
-        
-       st.push(src);
+       while(st.size()>0){
+           System.out.println(st.pop());
+       }
    }
-   public static void orderofcompilation(ArrayList<Edge> graph[]){
-      boolean visited[]=new boolean[graph.length];
-     Stack<Integer> st=new Stack<>();
-      for(int i=0;i<graph.length;i++){
-          if(visited[i]==false){
-              OFC(graph,i,visited,st);
-          }
-      }
-      while(st.size()>0){
-          System.out.println(st.pop());
-      }
-    
-   }
+   
+
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -50,7 +70,7 @@ public class Main {
          int v2 = Integer.parseInt(parts[1]);
          graph[v1].add(new Edge(v1, v2));
       }
-       orderofcompilation(graph);
+      OrderOfcompilation(graph);
       // write your code here
    }
 
--- Graphs/(17)HamilTonianPathAndCycle.java
+++ Graphs/(17)HamilTonianPathAndCycle.java
@@ -13,31 +13,52 @@ public class Main {
          this.wt = wt;
       }
    }
-  public static void HamilTonianCycle(ArrayList<Edge> graph[],int osrc,int vtx,HashSet<Integer> visited,String psf){
-      if(visited.size()==graph.length-1){
-          psf=psf+vtx;
-          boolean cycle=false;
-          for(Edge e:graph[vtx]){
-              if(e.nbr==osrc){
-                  cycle=true;
-                  break;
-              }
-          }
-          if(cycle){
-              System.out.println(psf+"*");
-          }
-          else{
-              System.out.println(psf+".");
-          }
-      }
-      visited.add(vtx);
-      for(Edge e:graph[vtx]){
-          if(!visited.contains(e.nbr)){
-             HamilTonianCycle(graph,osrc,e.nbr,visited,psf+vtx+"");
-          }
-      }
-      visited.remove(vtx);
-  }
+//   Sample Input
+// 7
+// 9
+// 0 1 10
+// 1 2 10
+// 2 3 10
+// 0 3 10
+// 3 4 10
+// 4 5 10
+// 5 6 10
+// 4 6 10
+// 2 5 10
+// 0
+// Sample Output
+// 0123456.
+// 0123465.
+// 0125643*
+// 0346521*
+    public static void HamiltonianPathAndCycle(ArrayList<Edge> graph[],int src,int osrc,HashSet<Integer> visited,String psf){
+        if(visited.size()==graph.length-1){
+            psf=psf+src;
+            //At top we are checking the size of visited,if size of visited is graph.length-1,then it is either hamiltoninan path\
+            //or cycle.To check if it is a cycle or not,we check the neighbours of our last vertex,if its one of neighbour is source vertrx
+            //then it is a hamiltonian cycle.
+            boolean iscycle=false;
+            for(Edge e:graph[src]){
+                if(e.nbr==osrc){
+                    iscycle=true;
+                    break;
+                }
+            }
+            if(iscycle){
+                System.out.println(psf+"*");
+            }
+            else{
+                System.out.println(psf+".");
+            }
+        }
+        visited.add(src);
+        for(Edge e:graph[src]){
+            if(!visited.contains(e.nbr)){
+               HamiltonianPathAndCycle(graph,e.nbr,osrc,visited,psf+src+"");
+            }
+        }
+        visited.remove(src);
+    }
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -58,7 +79,7 @@ public class Main {
       }
 
       int src = Integer.parseInt(br.readLine());
-      HamilTonianCycle(graph,src,src,new HashSet(),"");
+      HamiltonianPathAndCycle(graph,src,src,new HashSet<Integer>(),"");
       // write all your codes here
    }
 
